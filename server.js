6d922b16b3379e8233d70e215ea1289b3807ceb7
faizan1699 <faizanrasheed169@.com>
2024-10-03 09:36:31 -0700

    add check text git

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  // console.log("New client connected f:", socket.id);
+  // console.log("New client connected f:", s socket.id);
 
   socket.on("join_room", (roomId) => {
     socket.join(roomId);
